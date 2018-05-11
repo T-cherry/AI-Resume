c30779787fff55b6e52679e8f4630573184bd24a
TomC <psytc5@nottingham.ac.uk>
2018-05-11 11:31:31 +0100

    Cleaning up Repository

--- new version of the website/node_modules/watson-developer-cloud/tone-analyzer/v3.d.ts
+++
@@ -1,7 +0,0 @@
-import GeneratedToneAnalyzerV3 = require('./v3-generated');
-declare class ToneAnalyzerV3 extends GeneratedToneAnalyzerV3 {
-    constructor(options: any);
-    tone(params: any, callback: any): any;
-    tone_chat(params: any, callback: any): any;
-}
-export = ToneAnalyzerV3;
